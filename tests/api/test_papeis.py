debcea5ebdd93db1210b99538f51680da9795678
psenna <pedrosennapsc@gmail.com>
2021-11-19 16:01:11 -0300

    Adicionando os primeiros testes

---
+++ tests/api/test_papeis.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+from models.papel import Papel
+from tests.utils.papeis import create_papel_valido
+
+def test_cria_papel(client: TestClient) -> None:
+    body = create_papel_valido()
+    response = client.post("/papeis/", json=body)
+    content = response.json()
+    assert response.status_code == 200
+    assert content["cnpj"] == body["cnpj"]
+
---
+++ tests/utils/papeis.py
@@ -0,0 +1,7 @@
+def create_papel_valido():
+    return {
+        "id": 0,
+        "nome": "string",
+        "sigla": "string",
+        "cnpj": "string",
+    }
