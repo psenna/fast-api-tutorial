2103ca644cd148c871d1432f6116a1716adce114
psenna <pedrosennapsc@gmail.com>
2021-11-23 12:47:30 -0300

    Obter um papel por id

--- controllers/papeis_controller.py
+++ controllers/papeis_controller.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
+import ormar
 
 from models.papel import Papel
 
@@ -12,3 +13,12 @@ async def add_item(papel: Papel):
 @router.get("/")
 async def list_item():
     return await Papel.objects.all()
+
+@router.get("/{papel_id}")
+async def get_papel(papel_id: int, response: Response):
+    try:
+        papel = await Papel.objects.get(id=papel_id)
+        return papel
+    except ormar.exceptions.NoMatch:
+        response.status_code = 404
+        return {"mensagem": "Entidade não encontrada"}
--- tests/api/test_papeis.py
+++ tests/api/test_papeis.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
 from models.papel import Papel
 from tests.utils.papeis import create_papel_invalido, create_papel_valido
+import asyncio
 
 def test_cria_papel(client: TestClient) -> None:
     body = create_papel_valido()
@@ -15,3 +16,21 @@ def test_cria_papel_com_sigla_invalida(client: TestClient) -> None:
     content = response.json()
     assert response.status_code == 422
 
+def test_obtem_um_papel_por_id(client: TestClient) -> None:
+    atributos = create_papel_valido()
+    papel = Papel(**atributos)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(papel.save())
+
+    response = client.get(f"/papeis/{papel.id}")
+    content = response.json()
+
+    assert response.status_code == 200
+    assert content["sigla"] == papel.sigla
+
+def test_obtem_papel_inexistente_por_id(client: TestClient) -> None:
+    response = client.get(f"/papeis/1")
+    content = response.json()
+
+    assert response.status_code == 404
+    assert content["mensagem"] == "Entidade não encontrada"
